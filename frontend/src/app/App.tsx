da28b1ed61c775957227da4fbeb59db3c8911d6d
balazs.sipos@aliz.ai <balazs.sipos.hu@gmail.com>
2023-01-03 16:36:40 +0100

    feat: Add PageNotFound page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Auth } from "./auth/pages/Auth";
+import { PageNotFound } from "./PageNotFound";
 import { Todos } from "./todo/pages/Todos";
 import { Users } from "./user/pages/Users";
 
@@ -12,6 +13,7 @@ export const App = () => {
           <Route index element={<Users />} />
           <Route path={`:uid/todos`} element={<Todos />} />
           <Route path="auth" element={<Auth />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
