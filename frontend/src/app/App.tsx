9db2f8adcaabbd648d71186d02f032b7763c50c2
balazs.sipos@aliz.ai <balazs.sipos.hu@gmail.com>
2023-01-04 12:55:39 +0100

    feat:  Add user model, page and components

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,21 +1,27 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Auth } from "./auth/pages/Auth";
+import { useTheme } from "./common/theme/use-theme";
 import { PageNotFound } from "./PageNotFound";
 import { Todos } from "./todo/pages/Todos";
 import { Users } from "./user/pages/Users";
 
 export const App = () => {
+  const theme = useTheme()
   return (
     <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Users />} />
-          <Route path={`:uid/todos`} element={<Todos />} />
-          <Route path="auth" element={<Auth />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Users />} />
+            <Route path={`:uid/todos`} element={<Todos />} />
+            <Route path="auth" element={<Auth />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </StrictMode>
   );
 };
