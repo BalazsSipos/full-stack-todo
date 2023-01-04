3d4952aeaf439229d3e689563dab05da4623239e
balazs.sipos@aliz.ai <balazs.sipos.hu@gmail.com>
2023-01-04 14:25:55 +0100

    Add navigation header

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,26 +2,29 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Auth } from "./auth/pages/Auth";
+import { AppFrame } from "./common/components/AppFrame";
 import { useTheme } from "./common/theme/use-theme";
 import { PageNotFound } from "./PageNotFound";
 import { Todos } from "./todo/pages/Todos";
 import { Users } from "./user/pages/Users";
 
 export const App = () => {
-  const theme = useTheme()
+  const theme = useTheme();
   return (
     <StrictMode>
-      <CssBaseline />
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Users />} />
-            <Route path={`:uid/todos`} element={<Todos />} />
-            <Route path="auth" element={<Auth />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <AppFrame>
+            <Routes>
+              <Route index element={<Users />} />
+              <Route path={`:uid/todos`} element={<Todos />} />
+              <Route path="auth" element={<Auth />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </AppFrame>
+        </ThemeProvider>
+      </BrowserRouter>
     </StrictMode>
   );
 };
---
+++ frontend/src/app/common/components/AppFrame.tsx
@@ -0,0 +1,70 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Chip,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import PeopleIcon from "@mui/icons-material/People";
+import { TaskAlt } from "@mui/icons-material";
+
+const AppMenu = () => {
+  return (
+    <>
+      <Stack direction="row" alignItems="center" sx={{ backgroundColor: "important.main"}}>
+        <Typography
+          color="secondary"
+          variant="h3"
+          fontWeight={400}
+          sx={{ display: "inline-block", mx: 2 }}
+          gutterBottom={false}
+        >
+          TODO
+        </Typography>
+        <DevelopmentEnvIndicator />
+        <ButtonGroup variant="outlined" color="secondary" sx={{ p: 1 }}>
+          <Button component={Link} to="/" startIcon={<PeopleIcon />}>
+            Users
+          </Button>
+
+          <Button component={Link} to="/1/todos" startIcon={<TaskAlt />}>
+            My Todos
+          </Button>
+        </ButtonGroup>
+      </Stack>
+    </>
+  );
+};
+
+const DevelopmentEnvIndicator = () =>
+  process.env.NODE_ENV !== "production" ? (
+    <Chip
+      label={process.env.NODE_ENV}
+      variant="filled"
+      color="warning"
+      sx={{ m: 1 }}
+    />
+  ) : null;
+
+export const AppFrame = (props: React.PropsWithChildren<unknown>) => {
+  return (
+    <Stack
+      flexDirection="column"
+      rowGap={2}
+      sx={{
+        width: "100hw",
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <>
+        <AppMenu />
+        <Box sx={{ minWidth: "100%", minHeight: "100%", px: 2, py: 1 }}>
+          {props.children}
+        </Box>
+      </>
+    </Stack>
+  );
+};
