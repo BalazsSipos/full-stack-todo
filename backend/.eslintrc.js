097cf7b37fe4958485d33d4fc9d898205a4bc633
balazs.sipos@aliz.ai <balazs.sipos.hu@gmail.com>
2023-01-05 11:04:52 +0100

    feat: Add eslint and prettier dependencies

---
+++ backend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks', 'sort-imports-es6-autofix', 'risxss'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-shadow': 'warn',
+    'sort-imports-es6-autofix/sort-imports-es6': 'warn',
+    'risxss/catch-potential-xss-react': 'error',
+    'react/display-name': 'off',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+}
