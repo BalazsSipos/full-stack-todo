ad75d8b46db058f0dc7ffe1a8f99dcbbd05b8a49
balazs.sipos@aliz.ai <balazs.sipos.hu@gmail.com>
2023-01-30 16:17:24 +0100

    feat: Change @ import

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,16 +1,16 @@
-import { HttpException } from '@exceptions/HttpException';
-import { NextFunction, Request, Response } from 'express';
+import { HttpException } from 'exceptions/HttpException'
+import { NextFunction, Request, Response } from 'express'
 
 const errorMiddleware = (exception: HttpException, req: Request, res: Response, next: NextFunction) => {
   try {
-    const status: number = exception.status || 500;
-    const message: string = exception.message || 'Something went wrong';
+    const status: number = exception.status || 500
+    const message: string = exception.message || 'Something went wrong'
 
-    console.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
-    res.status(status).json({ message });
+    console.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`)
+    res.status(status).json({ message })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-export default errorMiddleware;
+export default errorMiddleware
