494061906dbd6f6716252942934ec23c1da6e3d6
Balazs Sipos <balazs.sipos.hu@gmail.com>
2024-04-17 10:17:16 +0200

    Add ORIGIN to cors header

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin';
 import { AppDataSource } from './data-source';
+import { ORIGIN } from './config';
 import errorMiddleware from './middlewares/error.middleware';
 import express from 'express';
 
@@ -18,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', ORIGIN);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
