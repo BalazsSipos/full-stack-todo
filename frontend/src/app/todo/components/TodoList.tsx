3551d9852f8a494c9a86631cd528c13a8df73978
Balazs Sipos <balazs.sipos.hu@gmail.com>
2024-05-08 17:13:29 +0200

    Show loading page while query loading

--- frontend/src/app/todo/components/TodoList.tsx
+++ frontend/src/app/todo/components/TodoList.tsx
@@ -1,9 +1,10 @@
 import { AuthContext } from '../../common/components/AuthContext';
 import { Card, CardContent, Grid, Typography } from '@mui/material';
 import { GlassSurface } from '../../common/components/GlassSurface';
+import { Loading } from '../../pages/Loading';
+import { ReactNode, useContext, useEffect, useState } from 'react';
 import { Todo } from '../models/Todo';
 import { TodoItem } from './TodoItem';
-import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useTodos } from '../../common/hooks/queries/use-todo';
 import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
@@ -32,28 +33,35 @@ export const TodoList = () => {
     }
   }, [firebaseUser]);
 
-  const todosQuery = useTodos(email ?? '', token);
+  const { data, isLoading } = useTodos(email ?? '', token);
 
-  const todoList = todosQuery.data ?? [];
+  let content: ReactNode;
 
-  return (
-    <>
-      {todoList.length ? (
+  if (isLoading) {
+    content = <Loading />;
+  }
+
+  if (data) {
+    if (!isLoading && data.length > 0) {
+      content = (
         <Grid
           container
           spacing={{ xs: 2, md: 4, lg: 8 }}
           // sx={{ p: { xs: 2, md: 4 } }}
           columns={{ xs: 1, md: 6, lg: 6, xl: 9 }}
         >
-          {todoList.map((todo: Todo) => (
+          {data.map((todo: Todo) => (
             <Grid item xs={1} md={3} key={todo.id} justifyContent="center">
               <TodoItem todo={todo} />
             </Grid>
           ))}
         </Grid>
-      ) : (
-        <EmptyListCard />
-      )}
-    </>
-  );
+      );
+    }
+    if (!isLoading && data.length == 0) {
+      content = <EmptyListCard />;
+    }
+  }
+
+  return content;
 };
